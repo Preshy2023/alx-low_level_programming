2c859da91be713fa46476738d915d34daf49def3
Preshy2023 <precious.mthethwa@icloud.com>
2023-02-17 12:04:42 -0800

    0-positive_or_negative.cc

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,11 +5,12 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 	int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 /* your code goes there */
