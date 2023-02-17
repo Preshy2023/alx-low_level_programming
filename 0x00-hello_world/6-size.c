2b89ede0875aba7d9696b1bac8d22fa447c93818
Preshy2023 <precious.mthethwa@icloud.com>
2023-02-17 13:48:49 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("Size of a char: %i bytes(s)\n", sizeo f(char));
+	printf("Size of a int: %i bytes(s)\n", sizeo f(int));
+	printf("Size of a long int: %i bytes(s)\n", sizeo f(long int));
+	printf("Size of a long long int: %i bytes(s)\n", sizeo f(long long int))
+		return (0)
+}
+
