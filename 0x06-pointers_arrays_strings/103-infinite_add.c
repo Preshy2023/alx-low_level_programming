9305d343ee00afd8c54672b5445fc76152146199
Preshy2023 <precious.mthethwa@icloud.com>
2023-03-02 02:41:58 -0800

    infinite

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -46,7 +46,6 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		j++;
 	i--;
 	j--;
-}
 	if (j >= size_r || i >= size_r)
 		return (0);
 	while (j >= 0 || i >= 0 || overflow == 1)
@@ -71,9 +70,4 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		j--;
 		i--;
 	}
-if (digits == size_r)
-	return (0);
-	*(r + digits) = '\0';
-	rev_string(r);
-	return (r);
-	}
+
